1743789686ae93b844366be792bd20986671356d
tasukumiyajima <miyajima.tasuku.21@gmail.com>
2020-08-23 22:27:27 +0900

    update facebook login

--- config/initializers/omniouth.rb
+++ config/initializers/omniouth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, Rails.application.credentials.facebook[:key], Rails.application.credentials.facebook[:secret]
 end
